a265472e1df399dc05e5109a3b5c8c71cbd58cc6
chkampawlow <104132629+chkampawlow@users.noreply.github.com>
2023-10-08 23:35:34 +0100

    prosit 3

---
+++ src/Controller/AuthorController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AuthorController extends AbstractController
+{
+    #[Route('/author', name: 'app_author')]
+    public function index(): Response
+    {
+        return $this->render('author/index.html.twig', [
+            'controller_name' => 'AuthorController',
+        ]);
+    }
+    #[Route("/author/show/{name}", name:"show_author")]
+    public function showAuthor($name): Response
+    {
+        return $this->render('author/show.html.twig', [
+            'name' => $name,
+        ]);
+    }
+    #[Route("/authorList", name:"listAuthor")]
+    public function list(): Response
+    {
+        $authors = [
+            [
+                'id' => 1,
+                'picture' => '/images/Victor-Hugo.jpg',
+                'username' => 'Victor Hugo',
+                'email' => 'victor.hugo@gmail.com',
+                'nb_books' => 100,
+            ],
+            [
+                'id' => 2,
+                'picture' => '/images/william-shakespeare.jpg',
+                'username' => 'William Shakespeare',
+                'email' => 'william.shakespeare@gmail.com',
+                'nb_books' => 200,
+            ],
+            [
+                'id' => 3,
+                'picture' => '/images/Taha_Hussein.jpg',
+                'username' => 'Taha Hussein',
+                'email' => 'taha.hussein@gmail.com',
+                'nb_books' => 300,
+            ],
+        ];
+
+        $totalAuthors = count($authors);
+        $totalBooks = array_sum(array_column($authors, 'nb_books'));
+
+        return $this->render('author/list.html.twig', [
+            'authors' => $authors,
+            'totalAuthors' => $totalAuthors,
+            'totalBooks' => $totalBooks,
+        ]);
+    }
+    #[Route("/authors/{id}", name:"author_details")]
+    public function authorDetails($id): Response
+    {
+        // Vous devrez récupérer les détails de l'auteur en fonction de l'ID
+        // Cela dépendra de la façon dont vous stockez ces données (base de données, tableau, etc.).
+
+        $author = [
+            'id' => $id,
+            'picture' => '/images/Victor-Hugo.jpg',
+            'username' => 'Victor Hugo',
+            'email' => 'victor.hugo@gmail.com',
+            'nb_books' => 100,
+        ];
+
+        return $this->render('author/showAuthor.html.twig', [
+            'author' => $author,
+        ]);
+    }
+}
---
+++ src/Controller/StudentController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class StudentController extends AbstractController
+{
+    #[Route('/students', name: 'student_index')]
+    public function index(): Response
+    {
+        return $this->render('student/index.html.twig', [
+            'controller_name' => 'StudentController',
+        ]);
+    }
+
+    #[Route('/show', name: 'show')]
+    public function show(): Response
+    {
+        return new Response('<h1>Bonjour Abdennour </h1>');
+    }
+}
